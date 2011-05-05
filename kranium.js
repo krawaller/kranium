d6d5131e71e4da2aff229bbb63a85f6f213fccdf
Jacob Waller <jacob@krawaller.se>
2011-05-05 08:45:46 +0200

    Fixing version getter and adding defaults

--- kranium.js
+++ kranium.js
@@ -3,6 +3,13 @@ var nomnom = require('nomnom'),
 	parser = nomnom(),
 	fs = require('fs');
 
+var defaults = {
+	port: 8128,
+	ip: '127.0.0.1',
+	test: false,
+	debug: false
+};
+
 function autoload(str){
 	return function(opts){
 		var fn = require(str);
@@ -16,7 +23,7 @@ parser.globalOpts({
 		string: '-v, --version',
 		help: 'print version and exit',
 		callback: function() {
-			return JSON.parse(fs.readFileSync('package.json')).version;
+			return JSON.parse(fs.readFileSync(require.main.filename.replace(/[^\/]+$/, 'package.json'))).version;
 		}
 	}
 });
@@ -24,12 +31,14 @@ parser.globalOpts({
 parser.command('init').opts({
 	debug: {
 		string: '-d, --debug',
-		help: 'set app to debug mode'
+		help: 'set app to debug mode',
+		"default": defaults.debug
 	},
 	
 	test: {
 		string: '-t, --test',
-		help: 'activate app tests'
+		help: 'activate app tests',
+		"default": defaults.test
 	},
 	
 	ip: {
@@ -39,7 +48,8 @@ parser.command('init').opts({
 	
 	port: {
 		string: '-p, --port',
-		help: 'port to bind to'
+		help: 'port to bind to',
+		"default": defaults.port
 	},
 }).callback(autoload('./lib/init'));
 
