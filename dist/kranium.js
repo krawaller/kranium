a4f8c09741b2cbc37e99454dbb5aceab8f0260d4
Jacob Waller <jacob.waller@logica.com>
2011-09-16 14:53:02 +0200

    Fix

--- dist/kranium.js
+++ dist/kranium.js
@@ -2,6 +2,7 @@
 /*!
  * Kranium
  * Copyright (c) 2011 Jacob Waller <jacob@krawaller.se>
+ * Copyright (c) 2011 David Waller <david@krawaller.se>
  * MIT Licensed
  * 
  * Portions of the copyright belongs to the following entities
@@ -993,7 +994,7 @@ K.parseJSON = JSON.parse;
 		} else {
 			Ti.UI.createNotification({
 				message: msg,
-				duration: Ti.UI.NOTIFICATION_DURATION_LONG,
+				duration: Ti.UI.NOTIFICATION_DURATION_SHORT,
 				offsetX: 0,
 				offsetY: 30
 			}).show();
@@ -1017,9 +1018,17 @@ K.parseJSON = JSON.parse;
 			hint = null;
 		}
 	
-		return (cache[key] = (Titanium.Locale.getString(key, hint)||'').replace(/(^|[^\w\d])@([A-Za-z_\-]+)\b/g, function($0, $1, name){
-			return ($1||'')+modifier(obj && typeof obj[name] !== 'undefined' ? obj[name] : Titanium.Locale.getString(name));
-		}));
+		var doCache = true,
+			ret = ((Titanium.Locale.getString(key, hint || key) || '').replace(/(^|[^\w\d])@([A-Za-z_\-]+)\b/g, function($0, $1, name){
+				doCache = false;
+				return ($1||'')+modifier(obj && typeof obj[name] !== 'undefined' ? obj[name] : Titanium.Locale.getString(name));
+			}));
+			
+		if(doCache){
+			cache[key] = ret;
+		}
+	
+		return ret;
 	};
 })();
 
@@ -1230,6 +1239,195 @@ String.prototype.esc = function(obj, func, matcher){
 //})(this);
 
 /*** QSA ***/
+/*!
+ * "mini" Selector Engine
+ * Copyright (c) 2009 James Padolsey
+ * -------------------------------------------------------
+ * Dual licensed under the MIT and GPL licenses.
+ *    - http://www.opensource.org/licenses/mit-license.php
+ *    - http://www.gnu.org/copyleft/gpl.html
+ * -------------------------------------------------------
+ * Version: 0.01 (BETA)
+ */
+
+$.qsa = $$ = (function(document, global){
+	
+	var me = document;
+	[
+		{ fn: "getElementsByClassName", arrName: "elsByClassName" },
+		{ fn: "getElementsByTagName", arrName: "elsByName" },
+		{ fn: "getElementById", arrName: "elsById" }
+	].forEach(function(o){
+		var name = o.fn, 
+			arrName = o.arrName,
+			singular = (o.fn == "getElementById"), 
+			a, 
+			res;
+
+		me[name] = global[name] = function(s, context){
+			var arr = K[arrName],
+				res = null;
+
+			if((a = arr[s]) && (a = (Array.isArray(a) ? a : [a]))){
+				if(context){
+					res = a.filter(function(el){
+						do {
+							if(el._uuid === context._uuid){ return true; }
+						} while((el = (el.getParent()) ));
+						return false;
+					});
+				} else {
+					res = a;
+				}
+			}
+
+			return singular ? Array.isArray(res) && res[0] : res;
+		};
+	});
+	
+	
+    var snack = /(?:[\w\-\\.#]+)+(?:\[\w+?=([\'"])?(?:\\\1|.)+?\1\])?|\*|>/ig,
+        exprClassName = /^(?:[\w\-_]+)?\.([\w\-_]+)/,
+        exprId = /^(?:[\w\-_]+)?#([\w\-_]+)/,
+        exprNodeName = /^([\w\*\-_]+)/,
+        na = [null,null];
+
+    function _find(selector, context) {
+        /**
+         * This is what you call via x()
+         * Starts everything off...
+         */
+        var simple = /^[\w\-_#]+$/.test(selector);
+		
+        if (selector.indexOf(',') > -1) {
+            var split = selector.split(/,/g), ret = [], sIndex = 0, len = split.length;
+            for(; sIndex < len; ++sIndex) {
+                ret = ret.concat( _find(split[sIndex], context) );
+            }
+            return unique(ret);
+        }
+
+        var parts = selector.match(snack),
+            part = parts.pop(),
+            id = (part.match(exprId) || na)[1],
+            className = !id && (part.match(exprClassName) || na)[1],
+            nodeName = !id && (part.match(exprNodeName) || na)[1],
+            collection,
+			el;
+
+        if (className && !nodeName) {
+            collection = realArray(getElementsByClassName(className, context));
+        } else {
+            collection = !id && realArray(getElementsByTagName(nodeName||'*', context));
+			if (className) {
+                collection = filterByAttr(collection, 'className', RegExp('(^|\\s)' + className + '(\\s|$)'));
+            }
+            if (id) {
+                return (el = getElementById(id, context)) ? [el] : [];
+            }
+        }
+		
+		var ret = parts[0] && collection[0] ? filterParents(parts, collection, false, context) : collection;
+		return ret;
+    }
+
+    function realArray(c) { return Array.prototype.slice.call(c); }
+
+    function filterParents(selectorParts, collection, direct, context) {
+        /**
+         * This is where the magic happens.
+         * Parents are stepped through (upwards) to
+         * see if they comply with the selector.
+         */
+
+        var parentSelector = selectorParts.pop()||'';
+        if (parentSelector === '>') { return filterParents(selectorParts, collection, true, context); }
+
+        var ret = [],
+            r = -1,
+            id = (parentSelector.match(exprId) || na)[1],
+            className = !id && (parentSelector.match(exprClassName) || na)[1],
+            nodeName = !id && (parentSelector.match(exprNodeName) || na)[1],
+            cIndex = -1,
+            node, parent,
+            matches;
+
+        while ( (node = collection[++cIndex]) ) {
+            if(context){
+				if(node.getParent()._uuid == context._uuid){
+					ret[++r] = node;
+				}
+			} else {
+				parent = node.getParent();
+	            do {
+	                matches = !nodeName || nodeName === '*' || nodeName === parent._type;
+	                matches = matches && (!id || parent._id === id);
+	                matches = matches && (!className || RegExp('(^|\\s)' + className + '(\\s|$)').test(parent.className));
+	                if (direct || matches) { break; }
+	            } while ( (parent = parent.getParent()) );
+	            if (matches) { ret[++r] = node; }
+			}
+			
+        }
+        return selectorParts[0] && ret[0] ? filterParents(selectorParts, ret) : ret;
+    }
+
+
+    var unique = (function() {
+		var uid = +new Date(),
+			data = (function() {
+
+			var n = 1;
+			return function(elem) {
+				var cacheIndex = elem[uid],
+					nextCacheIndex = n++;
+
+				if (!cacheIndex) {
+					elem[uid] = nextCacheIndex;
+					return true;
+				}
+				return false;
+			};
+
+		})();
+
+		return function(arr) {
+			/**
+			 * Returns a unique array
+			 */
+			var length = arr.length,
+				ret = [],
+				r = -1,
+				i = 0,
+				item;
+
+			for (; i < length; ++i) {
+				item = arr[i];
+				if (data(item)) { ret[++r] = item; }
+			}
+
+			uid += 1;
+			return ret;
+		};
+	})();
+
+    function filterByAttr(collection, attr, regex) {
+        /**
+         * Filters a collection by an attribute.
+         */
+        var i = -1, node, r = -1, ret = [];
+        while ( (node = collection[++i]) ) {
+            if (regex.test(node[attr])) {
+                ret[++r] = node;
+            }
+        }
+        return ret;
+    }
+    return _find;
+
+})(this, this);
+
+/*** KLASS ***/
 /* Simple JavaScript Inheritance
  * By John Resig http://ejohn.org/
  * MIT Licensed.
@@ -1324,7 +1522,7 @@ String.prototype.esc = function(obj, func, matcher){
 
 })(this);
 
-/*** KLASS ***/
+/*** FILE ***/
 /**
  * Define file module
  */
@@ -1375,7 +1573,7 @@ String.prototype.esc = function(obj, func, matcher){
 
 })();
 
-/*** FILE ***/
+/*** STYLE ***/
 /**
  * Define style module
  */
@@ -1673,7 +1871,7 @@ String.prototype.esc = function(obj, func, matcher){
 
 })(this);
 
-/*** STYLE ***/
+/*** CREATE ***/
 /**
  * Define create module
  */
@@ -2192,7 +2390,7 @@ String.prototype.esc = function(obj, func, matcher){
 	
 })(this);
 
-/*** CREATE ***/
+/*** AJAX ***/
 /**
  * Define AJAX module
  */
@@ -2278,7 +2476,6 @@ String.prototype.esc = function(obj, func, matcher){
 							catch(e){
 								Ti.API.error(e);
 								Ti.API.error(text);
-								throw "WTF?!"+e;
 							}
 							break;
 
@@ -2365,195 +2562,6 @@ String.prototype.esc = function(obj, func, matcher){
 	};
 
 })(this);
-
-/*** AJAX ***/
-/*!
- * "mini" Selector Engine
- * Copyright (c) 2009 James Padolsey
- * -------------------------------------------------------
- * Dual licensed under the MIT and GPL licenses.
- *    - http://www.opensource.org/licenses/mit-license.php
- *    - http://www.gnu.org/copyleft/gpl.html
- * -------------------------------------------------------
- * Version: 0.01 (BETA)
- */
-
-$.qsa = $$ = (function(document, global){
-	
-	var me = document;
-	[
-		{ fn: "getElementsByClassName", arrName: "elsByClassName" },
-		{ fn: "getElementsByTagName", arrName: "elsByName" },
-		{ fn: "getElementById", arrName: "elsById" }
-	].forEach(function(o){
-		var name = o.fn, 
-			arrName = o.arrName,
-			singular = (o.fn == "getElementById"), 
-			a, 
-			res;
-
-		me[name] = global[name] = function(s, context){
-			var arr = K[arrName],
-				res = null;
-
-			if((a = arr[s]) && (a = (Array.isArray(a) ? a : [a]))){
-				if(context){
-					res = a.filter(function(el){
-						do {
-							if(el._uuid === context._uuid){ return true; }
-						} while((el = (el.getParent()) ));
-						return false;
-					});
-				} else {
-					res = a;
-				}
-			}
-
-			return singular ? Array.isArray(res) && res[0] : res;
-		};
-	});
-	
-	
-    var snack = /(?:[\w\-\\.#]+)+(?:\[\w+?=([\'"])?(?:\\\1|.)+?\1\])?|\*|>/ig,
-        exprClassName = /^(?:[\w\-_]+)?\.([\w\-_]+)/,
-        exprId = /^(?:[\w\-_]+)?#([\w\-_]+)/,
-        exprNodeName = /^([\w\*\-_]+)/,
-        na = [null,null];
-
-    function _find(selector, context) {
-        /**
-         * This is what you call via x()
-         * Starts everything off...
-         */
-        var simple = /^[\w\-_#]+$/.test(selector);
-		
-        if (selector.indexOf(',') > -1) {
-            var split = selector.split(/,/g), ret = [], sIndex = 0, len = split.length;
-            for(; sIndex < len; ++sIndex) {
-                ret = ret.concat( _find(split[sIndex], context) );
-            }
-            return unique(ret);
-        }
-
-        var parts = selector.match(snack),
-            part = parts.pop(),
-            id = (part.match(exprId) || na)[1],
-            className = !id && (part.match(exprClassName) || na)[1],
-            nodeName = !id && (part.match(exprNodeName) || na)[1],
-            collection,
-			el;
-
-        if (className && !nodeName) {
-            collection = realArray(getElementsByClassName(className, context));
-        } else {
-            collection = !id && realArray(getElementsByTagName(nodeName||'*', context));
-			if (className) {
-                collection = filterByAttr(collection, 'className', RegExp('(^|\\s)' + className + '(\\s|$)'));
-            }
-            if (id) {
-                return (el = getElementById(id, context)) ? [el] : [];
-            }
-        }
-		
-		var ret = parts[0] && collection[0] ? filterParents(parts, collection, false, context) : collection;
-		return ret;
-    }
-
-    function realArray(c) { return Array.prototype.slice.call(c); }
-
-    function filterParents(selectorParts, collection, direct, context) {
-        /**
-         * This is where the magic happens.
-         * Parents are stepped through (upwards) to
-         * see if they comply with the selector.
-         */
-
-        var parentSelector = selectorParts.pop()||'';
-        if (parentSelector === '>') { return filterParents(selectorParts, collection, true, context); }
-
-        var ret = [],
-            r = -1,
-            id = (parentSelector.match(exprId) || na)[1],
-            className = !id && (parentSelector.match(exprClassName) || na)[1],
-            nodeName = !id && (parentSelector.match(exprNodeName) || na)[1],
-            cIndex = -1,
-            node, parent,
-            matches;
-
-        while ( (node = collection[++cIndex]) ) {
-            if(context){
-				if(node.getParent()._uuid == context._uuid){
-					ret[++r] = node;
-				}
-			} else {
-				parent = node.getParent();
-	            do {
-	                matches = !nodeName || nodeName === '*' || nodeName === parent._type;
-	                matches = matches && (!id || parent._id === id);
-	                matches = matches && (!className || RegExp('(^|\\s)' + className + '(\\s|$)').test(parent.className));
-	                if (direct || matches) { break; }
-	            } while ( (parent = parent.getParent()) );
-	            if (matches) { ret[++r] = node; }
-			}
-			
-        }
-        return selectorParts[0] && ret[0] ? filterParents(selectorParts, ret) : ret;
-    }
-
-
-    var unique = (function() {
-		var uid = +new Date(),
-			data = (function() {
-
-			var n = 1;
-			return function(elem) {
-				var cacheIndex = elem[uid],
-					nextCacheIndex = n++;
-
-				if (!cacheIndex) {
-					elem[uid] = nextCacheIndex;
-					return true;
-				}
-				return false;
-			};
-
-		})();
-
-		return function(arr) {
-			/**
-			 * Returns a unique array
-			 */
-			var length = arr.length,
-				ret = [],
-				r = -1,
-				i = 0,
-				item;
-
-			for (; i < length; ++i) {
-				item = arr[i];
-				if (data(item)) { ret[++r] = item; }
-			}
-
-			uid += 1;
-			return ret;
-		};
-	})();
-
-    function filterByAttr(collection, attr, regex) {
-        /**
-         * Filters a collection by an attribute.
-         */
-        var i = -1, node, r = -1, ret = [];
-        while ( (node = collection[++i]) ) {
-            if (regex.test(node[attr])) {
-                ret[++r] = node;
-            }
-        }
-        return ret;
-    }
-    return _find;
-
-})(this, this);
 
 /*** LIVE ***/
 /**
@@ -2799,16 +2807,6 @@ $.qsa = $$ = (function(document, global){
 
 /*** STRINGIFY ***/
 /**
- * Define end module
- */
-
-(function(global){
-	Ti.include('/kranium/kranium-generated-bootstrap.js');
-})(this);
-
-
-/*** END ***/
-/**
  * Define stringify module
  */
 
@@ -2906,6 +2904,16 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 	K.stringify = stringify;
 
 })(this);
+
+/*** END ***/
+/**
+ * Define end module
+ */
+
+(function(global){
+	Ti.include('/kranium/kranium-generated-bootstrap.js');
+})(this);
+
 
 /*** TESTER ***/
 /**
--- dist/test/tests.js
+++ dist/test/tests.js
@@ -3,9 +3,6 @@
 		Ti.include('/kranium/lib/test/jasmine-1.0.2.js');
 		Ti.include('/kranium/lib/test/jasmine-titanium-node.js');
 		
-		// Include all the test files
-		Ti.include('/test/core.js');
-		
 		jasmine.getEnv().addReporter(new jasmine.TitaniumNodeReporter());
 		jasmine.getEnv().execute();
 	}
